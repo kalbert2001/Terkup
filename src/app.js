ce833925db728ce6f486aeb7e30a66aa406c4d33
Kiss Albert <youremail@asd.com>
2023-01-31 19:48:26 +0100

    Fejresz hozzaadva

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
+/*
+* File: app.js
+* Author: Kiss Albert Bence
+* Copyright: 2021, Kiss Albert Bence
+* Group: Szoft I-1/E
+* Date: 2023-01-31
+* Github: https://github.com/kisalb.88/
+* Licenc: GNU GPL
+*/
 const RadiusE = document.querySelector("#radius")
 const HeightE = document.querySelector("#height")
 const ButtonE = document.querySelector("#calcButton")
